2ba2462df433474733a7babd41cedba2fbb7897d
Leo <>
2021-04-26 17:24:05 -0300

    first commit - ofxAudioAnalyzer working in JUCE

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,41 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+EssentiaTestAudioProcessorEditor::EssentiaTestAudioProcessorEditor (EssentiaTestAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+EssentiaTestAudioProcessorEditor::~EssentiaTestAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void EssentiaTestAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void EssentiaTestAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
